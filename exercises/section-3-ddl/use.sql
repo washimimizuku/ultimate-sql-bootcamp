530064a93f14e20a1dfc430cc6284a089c7d18d9
Nuno Barreto <nfb@amazon.ch>
2025-12-09 11:31:20 +0100

    Add USE statement examples for database and schema

--- exercises/section-3-ddl/use.sql
+++ exercises/section-3-ddl/use.sql
@@ -1,8 +1,11 @@
+-- Databases
+
 SELECT current_database();
 -- CREATE DATABASE DEMO_DB
 -- USE DATABASE DEMO_DB;
 USE sample; -- Switch back to sample database
 
+--Schemas
 SELECT current_schema();
 CREATE SCHEMA demo_schema; -- Create a new schema
 USE demo_schema; -- Switch to the new schema
